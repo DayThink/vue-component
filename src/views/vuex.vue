1edb8f7782c6c706212343621638264796d075c8
zhengguangguo <zhengguangguo@bytedance.com>
2018-10-20 20:27:14 +0800

    1.局部this的作用域； 2.es2016语法中...深拷贝 3.mapGetter == this.$store.getters + ...(深拷贝)

--- src/views/vuex.vue
+++ src/views/vuex.vue
@@ -4,10 +4,14 @@
     <!--<button @click="update">获取Vuex全局变量值</button>-->
     <vuex-a></vuex-a>
     <vuex-b></vuex-b>
+
+    <h5>{{ city }}</h5>
+    <button @click="setCity">局部this</button>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import vuexA from '@/components/vuex-a'
 import vuexB from '@/components/vuex-b'
 export default {
@@ -21,7 +25,27 @@ export default {
     update () {
       this.logo = this.$store.getters.getLogo
       // this.$store.dispatch('setLogo', this.logo)
+    },
+    setCity () {
+      // 模拟请求2s,演示局部this坑
+      // setTimeout(function (resp) {
+      //   console.log(this)
+      //   this.$store.dispatch('setCityName', resp.cityName)
+      // }, 1000, {cityName: 'beijing'})
+      // let that = this
+      // setTimeout(function (resp) {
+      //   console.log(this)
+      //   that.$store.dispatch('setCityName', resp.cityName)
+      // }, 1000, {cityName: 'beijing'})
+      setTimeout((resp) => {
+        this.$store.dispatch('setCityName', resp.cityName)
+      }, 1000, {cityName: 'beijing'})
     }
+  },
+  computed: {
+    ...mapGetters({
+      city: 'getCityFn'
+    })
   },
   components: {
     vuexA,
